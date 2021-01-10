698c0fcf7b923a0c6125ee7f22953c176643462a
lheia <lasse.heia@basefarm.com>
2021-01-10 04:17:29 +0100

    Deploy virtual machine from ova image.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Terraform ESXI
+## Prerequisites
+* Ansible
+* VMware OVF Tool: https://www.vmware.com/support/developer/ovf/ (Select a release verison and press Software Download) Then `chmod 755 VMware-ovftool-4.4.1-16812187-lin.x86_64.bundle` and `sudo ./VMware-ovftool-4.4.1-16812187-lin.x86_64.bundle`
--- main.tf
+++ main.tf
@@ -1,12 +1,16 @@
-terraform {
-  required_version = ">= 0.13"
-  required_providers {
-    esxi = {
-      source = "registry.terraform.io/josenk/esxi"
-      #
-      # For more information, see the provider source documentation:
-      # https://github.com/josenk/terraform-provider-esxi
-      # https://registry.terraform.io/providers/josenk/esxi
-    }
+provider "esxi" {
+  esxi_hostname = var.esxi_hostname
+  esxi_hostport = var.esxi_hostport
+  esxi_hostssl  = var.esxi_hostssl
+  esxi_username = var.esxi_username
+  esxi_password = var.esxi_password
+}
+
+resource "esxi_guest" "vmtest02" {
+  guest_name         = "vmtest02"
+  disk_store         = "datastore1"
+  ovf_source         = "https://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-amd64.ova"
+  network_interfaces {
+    virtual_network = "VM-Lab"
   }
 }
